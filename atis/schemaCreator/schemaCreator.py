8f832c1d095e0fa9da4f98312d0f10c29e4f23dc
Aryan <aryan.singh@iitg.ac.in>
2024-10-26 16:08:28 +0530

    Minor Changes

--- atis/schemaCreator/schemaCreator.py
+++ atis/schemaCreator/schemaCreator.py
@@ -43,41 +43,63 @@ with open(trainNL_path, "r") as f:
 # Initialize dictionaries
 combined = {}
 
-# Create schema
-def createSchema():
+def extract_table_names(sql_query, table_list):
+    # Convert the SQL query to lowercase
+    sql_query_lower = sql_query.lower()
+
+    # Extract table names using map and filter, and store them in a set to avoid duplicates
+    extracted_tables_set = set(filter(lambda table: table in sql_query_lower, map(str.lower, table_list)))
+
+    # Convert the set back to a list to maintain the order and return it
+    extracted_tables_list = list(extracted_tables_set)
+
+    return extracted_tables_list
+
+table_list = ["aircraft", "airline", "airport", "airport_service", "city", "class_of_service", "code_description", "compartment_class", "date_day", "days", "dual_carrier", "equipment_sequence", "fare", "fare_basis", "flight", "flight_fare", "flight_leg", "flight_stop", "food_service", "ground_service", "month", "restriction", "state", "time_interval", "time_zone"]
+
+# Create data
+def createData():
     j = 0
     for i in range(len(devSQL)):
+        tables = extract_table_names(devSQL[i], table_list)
         combined[j] = {
             "question": devNL[i].strip(),
-            "query": devSQL[i].strip()
+            "query": devSQL[i].strip(),
+            "tables": tables
         }
         j += 1
     for i in range(len(testSQL)):
+        tables = extract_table_names(testSQL[i], table_list)
         combined[j] = {
             "question": testNL[i].strip(),
-            "query": testSQL[i].strip()
+            "query": testSQL[i].strip(),
+            "tables": tables
         }
         j += 1
     for i in range(len(train_devSQL)):
+        tables = extract_table_names(train_devSQL[i], table_list)
         combined[j] = {
             "question": train_devNL[i].strip(),
-            "query": train_devSQL[i].strip()
+            "query": train_devSQL[i].strip(),
+            "tables": tables
         }
         j += 1
     for i in range(len(trainSQL)):
+        tables = extract_table_names(trainSQL[i], table_list)
         combined[j] = {
             "question": trainNL[i].strip(),
-            "query": trainSQL[i].strip()
+            "query": trainSQL[i].strip(),
+            "tables": tables
         }
         j += 1
 
-createSchema()
+createData()
 
-
+saving_path = os.path.join(script_dir, "../../../ATIS/combined.json")
 
 print(len(combined))
 
-with open("../../../ATIS/combined.json", "w") as json_file:
+with open(saving_path, "w") as json_file:
     json.dump(combined, json_file, indent=4)
 
 print("Data saved")
